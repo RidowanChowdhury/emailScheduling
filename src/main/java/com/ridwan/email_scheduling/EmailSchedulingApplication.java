58ffc0db00559f10a7b0e55eec4d1c64d71b8b6f
RidwanBracu <you@example.com>
2019-10-16 14:35:10 +0600

    First commit

---
+++ src/main/java/com/ridwan/email_scheduling/EmailSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.ridwan.email_scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSchedulingApplication.class, args);
+	}
+
+}
